b5fd5b989de3ca2435449dcf5420d9c83ea69797
HAXA-XC <86255713+EBLETSM@users.noreply.github.com>
2021-09-01 17:25:27 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#Coder_CipherX
+echo "CipherX install Python"
+pkg install python
+pip install os
+pip install requests
+pip install colorama
+chmod 777 install.sh
+bash install.sh
+# cd sms # pkg install git # git clone https://github.com/EBLETSM/sms
